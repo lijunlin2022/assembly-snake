954a980c7cbcb79ce13414673849bfbc3c5e9c1e
a-layfolk <60834609+a-layfolk@users.noreply.github.com>
2021-05-17 10:42:18 +0800

    创建汇编指令

---
+++ snake.asm
@@ -0,0 +1,138 @@
+TITLE Program Snake
+
+.386
+.model flat, stdcall
+option casemap:none
+includelib msvcrt.lib
+
+ExitProcess proto, dwExitCode:dword
+Sleep proto, timeSpan:dword
+system proto C, :ptr sbyte, :vararg
+printf proto C :ptr sbyte, :vararg
+scanf proto C :ptr sbyte, :vararg
+memset proto C
+.data
+;----------------------------------------------showMainMenu()
+dividingLine byte "-------------------------------------------------------------------", 0ah, 0
+authorInfo byte "author: LiJunLin", 0ah, 0
+operationGuide byte "按 1 开始游戏", 0ah, "按 2 结束游戏", 0ah, "按 W 向上移动",  0ah, "按 S 向下移动", 0ah, "按 A 向左移动", 0ah, "按 D 向右移动", 0ah, 0
+
+;---------------------------------------------enterGame()
+paramater byte "%d", 0
+selection dword ?
+
+;----------------------------------------------handleIllegalSelection()
+errMsg byte "输入的编号不正确，请在 2s 之后重新输入", 0ah, 0
+
+;----------------------------------------------clearScreenUtil()
+clearScreen byte "cls", 0
+
+.code
+
+;------------------------------------------------------
+clearScreenUtil proc
+	mov eax, dword ptr offset clearScreen
+	push eax
+	call system
+	add esp, 4
+	ret
+clearScreenUtil endp
+;------------------------------------------------------
+
+;------------------------------------------------------
+handleIllegalSelection proc
+	; 打印错误信息
+	mov eax, dword ptr offset errMsg
+	push eax
+	call printf
+	add esp, 4
+	
+	; 延时函数
+	push 2000
+	call dword ptr offset Sleep
+	call clearScreenUtil
+	ret
+handleIllegalSelection endp
+;------------------------------------------------------
+
+
+;------------------------------------------------------
+
+;------------------------------------------------------
+initMapData proc
+
+initMapData endp
+;------------------------------------------------------
+startGame proc
+
+  ret
+startGame endp
+;------------------------------------------------------
+
+
+;------------------------------------------------------
+showMainMenu proc
+	; 打印分割字符
+	mov eax, dword ptr offset dividingLine
+	push eax
+	call printf
+	add esp, 4
+
+	; 打印作者信息
+	mov eax, dword ptr offset authorInfo
+	push eax
+	call printf
+	add esp, 4
+
+	; 打印提示信息
+	mov eax, dword ptr offset operationGuide
+	push eax
+	call printf
+	add esp, 4
+
+	; 打印分割字符
+	mov eax, dword ptr offset dividingLine
+	push eax
+	call printf
+	add esp, 4
+
+	ret
+showMainMenu endp
+;------------------------------------------------------
+
+;------------------------------------------------------
+enterGame proc
+	call showMainMenu
+	
+	; 获取用户选择
+	lea eax, dword ptr offset selection
+	push eax
+	mov ecx, dword ptr offset paramater
+	push ecx
+	call scanf
+	add esp, 8
+
+	; 判断用户的选择
+	mov eax, dword ptr offset selection
+	cmp eax, 1
+	je start_game
+	cmp eax, 2
+	je end_game
+
+	; 处理非法输入
+	call handleIllegalSelection
+
+start_game:
+	invoke printf, offset dividingLine
+end_game:
+	invoke printf, offset dividingLine
+	invoke printf, offset dividingLine
+	invoke ExitProcess, 0
+	ret
+enterGame endp
+;------------------------------------------------------
+
+main proc
+	call enterGame
+main endp
+end main
